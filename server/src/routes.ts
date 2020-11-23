232749c84fd6ec38b8e4435e0dc7549aeb208280
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-11-22 22:45:48 -0300

    initial project structure started

---
+++ server/src/routes.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+
+
+const routes = express.Router()
+
+//routes.get('/classes', classesController.index)
+//routes.post('/classes', classesController.create)
+
+//routes.get('/connections', connectionsController.index)
+//routes.post('/connections', connectionsController.create)
+
+export default routes
