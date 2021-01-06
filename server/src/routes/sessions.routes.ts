99800fe856dcf7dd8f532a2264a1eaec25e55689
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2021-01-05 22:26:08 -0300

    Added user authenticate

---
+++ server/src/routes/sessions.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+
+import AuthenticateUserService from '../services/AuthenticateUserService'
+
+const SessionsRouter = Router()
+
+SessionsRouter.post('/', async (request, response) => {
+	try {
+		const { email, password } = request.body
+
+		const authenticateUser = new AuthenticateUserService()
+
+		const { user, token } = await authenticateUser.execute({
+			email,
+			password
+		})
+
+		delete user.password
+
+		return response.json({ user, token })
+
+	} catch (err) {
+		return response.status(400).json({ error: err.message })
+	}
+
+})
+
+export default SessionsRouter
